1663b9dd2515acbcc55fcacb07ff5dd410cc63c0
Vincent Niehues <vniehues@icloud.com>
2021-11-03 18:18:37 +0100

    Initial Commit

---
+++ src/mavvm/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace mavvm
+{
+	// All the code in this file is included in all platforms.
+	public class Class1
+	{
+	}
+}
---
+++ src/mavvm/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace mavvm
+{
+	// All the code in this file is only included on Mac Catalyst.
+	public class PlatformClass1
+	{
+	}
+}
